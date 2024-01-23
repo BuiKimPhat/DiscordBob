3421c6a71eaa5ebcea021091ec86887dfda93ee8
BuiKimPhat <buikimphat42@gmail.com>
2024-01-23 21:27:54 +0700

    fix minor bugs

--- commands/utility/speaker.js
+++ commands/utility/speaker.js
@@ -6,12 +6,12 @@ module.exports = {
 		.setName('setspeaker')
 		.setDescription('Set Bob\'s voice.')
         .addStringOption(option =>
-            option.setName('speakerIDX')
+            option.setName('speakeridx')
                 .setDescription('SpeakerIDX: p225 -> p376')
                 .setRequired(true)),
 	async execute(interaction) {
 		// interaction.guild is the object representing the Guild in which the command was run
-        const speaker = interaction.options.getString("speakerIDX");
+        const speaker = interaction.options.getString("speakeridx");
         Bob.instance.setSpeakerIdx(speaker);
 		await interaction.reply(`Bob's speaker has been set to ${speaker}`);
 	},
---
+++ python/gpt.py
@@ -0,0 +1,4 @@
+from gpt4all import GPT4All
+model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
+output = model.generate("The capital of France is ", max_tokens=3)
+print(output)
